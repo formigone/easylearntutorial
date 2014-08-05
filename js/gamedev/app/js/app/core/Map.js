f9ad0dac98568c00da10afd45a490796efe7d0a2
Rodrigo Silveira <rsilveira@deseretdigital.com>
2014-08-05 16:10:14 -0600

    Implement map, map parser, map renderer

--- js/gamedev/app/js/app/core/Map.js
+++ js/gamedev/app/js/app/core/Map.js
@@ -1,5 +1,8 @@
 goog.provide('elt.core.Map');
 
+goog.require('elt.core.MapTile');
+goog.require('goog.dom');
+
 /**
  *
  * @type {number}width
@@ -19,6 +22,29 @@ elt.core.Map = function(width, height, renderer, tileType) {
     this.tileType = tileType || {};
 };
 
+elt.core.Map.fromJSON = function(obj, renderer) {
+    var map = new elt.core.Map(obj.width, obj.height, renderer, obj.tileTypes);
+
+    for (var i = 0, len = obj.layers.length; i < len; i++) {
+        var atlas = [];
+        for (var w = 0; w < obj.layers[i].atlas.length; w++) {
+            atlas.push(new elt.core.MapTile(obj.layers[i].atlas[w][0], obj.layers[i].atlas[w][1], obj.layers[i].atlas[w][2]));
+        }
+
+        map.addLayer(new elt.core.MapLayer(
+            obj.layers[i].width,
+            obj.layers[i].height,
+            obj.layers[i].tileWidth,
+            obj.layers[i].tileHeight,
+            goog.dom.createDom('img', {src: obj.layers[i].img}),
+            atlas,
+            obj.layers[i].tiles
+        ));
+    }
+
+    return map;
+};
+
 elt.core.Map.prototype.addLayer = function(layer) {
     this.layers.push(layer);
 };
@@ -28,15 +54,17 @@ elt.core.Map.prototype.setObjLayer = function(layer) {
 };
 
 elt.core.Map.prototype.render = function(now) {
-};
+    var ctx = this.renderer.ctx;
 
-elt.core.Map.prototype.fromJSON = function(obj, renderer) {
-    var map = new elt.core.Map(obj.width, obj.height, renderer, obj.tileTypes);
+    for (var layer = null, i = 0, len = this.layers.length; i < len; i++) {
+        layer = this.layers[i];
+        for (var w = 0, wLen = layer.tiles.length; w < wLen; w++) {
+            var x = w % layer.width;
+            var y = parseInt(w / layer.width, 10);
 
-    for (var i = 0, len = obj.layers.length; i < len; i++) {
-        // TODO: parse layers + create IMG + create Tiles
-        map.addLayer();
+            ctx.drawImage(layer.img,
+                layer.atlas[layer.tiles[w]].x, layer.atlas[layer.tiles[w]].y, layer.tileWidth, layer.tileHeight,
+                x * layer.tileWidth, y * layer.tileHeight, layer.tileWidth, layer.tileHeight);
+        }
     }
-
-    return map;
 };
