f51bfb3ffa83ec0c60a6dc3a00a807e21f891e0f
Rodrigo Silveira <rsilveira@deseretdigital.com>
2014-08-07 16:34:29 -0600

    Rough viewport rendering. Only account for correct windows if camera.y == 0

--- js/gamedev/app/js/app/core/Map.js
+++ js/gamedev/app/js/app/core/Map.js
@@ -53,14 +53,71 @@ elt.core.Map.prototype.setObjLayer = function(layer) {
     this.objLayer = layer;
 };
 
+
+/*
+
++---+
+| | |
++---+
+| | |
++---+
+w: 2, h: 2, x: 0, y: 0
+0, 1, 10, 11
+
+[ 0][ 1][ 2][ 3][ 4][ 5][6 ][ 7][ 8][ 9]
+[10][11][12][13][14][15][16][17][18][19]
+[20][21][22][23][24][25][26][27][28][29]
+[30][31][32][33][34][35][36][37][38][39]
+[40][41][42][43][44][45][46][47][48][49]
+
+w: 10, h: 5
+
+Coffset = c.y * c.w + c.x = (0 * 2 + 0) = 0
+Coffset * y + (i % c.w)
+
+[1, 2, 11, 22]
+w.w = 10, w.h = 5
+c.x = 1, c.y = 0;
+y = parseInt(i / c.w)
+Coffset = c.y * c.w + c.x = (0 * 2 + 1) = 1
+
+y = 0 / 2 = 0
+Tile[0] = Coffset + (0 * w.w) + (i % c.w) = 1 + 0 + 0 = 1
+
+y = 1 / 2 = 0
+Tile[1] = Coffset + (0 * w.w) + (i % c.w) = 1 + 0 + 1 = 2
+
+y = 2 / 2 = 1
+Tile[2] = Coffset + (1 * w.w) + (i % c.w) = 1 + 10 + 0 = 11
+
+y = 2 / 2 = 1
+Tile[3] = Coffset + (1 * w.w) + (i % c.w) = 1 + 10 + 1 = 12
+
+ */
+
+
 elt.core.Map.prototype.render = function(now) {
     var ctx = this.renderer.ctx;
     var layer = null;
-var cX = 1, cY = 0, cW = 5, cH = 3;
+    var x = 0;
+    var y = 0;
+    var offset = 0;
+    var camera = {
+        w: 5,
+        h: 3,
+        x: 1,
+        y: 0,
+        offset: 0
+    };
+
+    camera.offset = camera.y * camera.w + camera.x;
+
     for (var i = 0, len = this.layers.length; i < len; i++) {
         layer = this.layers[i];
 
-        for (var w = 0, wLen = Math.min(layer.tiles.length, cW * cH); w < wLen; w++) {
+        for (var w = 0, wLen = Math.min(layer.tiles.length, camera.w * camera.h); w < wLen; w++) {
+//        for (var w = 0, wLen = Math.max(layer.tiles.length, cW * cH); w < wLen; w++) {
+            /*
             // TODO: Get right sW -- this is not taking Y offset into account
             var sW = parseInt(cX + w, 10) % (layer.width - cW);
             cY = parseInt(sW / layer.width, 10);
@@ -68,10 +125,17 @@ var cX = 1, cY = 0, cW = 5, cH = 3;
             console.log(sW, cY, w);
             var dX = w % layer.width;
             var dY = parseInt(w / layer.width, 10);
+            */
+            y = parseInt(w / camera.w, 10);
+            x = w % camera.w;
+            offset = camera.offset + (y * layer.width) + x;
 
             ctx.drawImage(layer.img,
-                layer.atlas[layer.tiles[sW]].x, layer.atlas[layer.tiles[sW]].y, layer.tileWidth, layer.tileHeight,
-                dX * layer.tileWidth, dY * layer.tileHeight, layer.tileWidth, layer.tileHeight);
+                layer.atlas[layer.tiles[offset]].x, layer.atlas[layer.tiles[offset]].y, layer.tileWidth, layer.tileHeight,
+                x * layer.tileWidth, y * layer.tileHeight, layer.tileWidth, layer.tileHeight);
+//            ctx.drawImage(layer.img,
+//                layer.atlas[layer.tiles[w]].x, layer.atlas[layer.tiles[w]].y, layer.tileWidth, layer.tileHeight,
+//                dX * layer.tileWidth, dY * layer.tileHeight, layer.tileWidth, layer.tileHeight);
         }
     }
 };
